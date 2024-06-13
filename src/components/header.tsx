e712fde3ba06f42faa6b8683dfa004eabc27eb13
Garnet3106 <private@garnet.works>
2024-06-14 06:56:47 +0900

    Fix header UI

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,13 @@
 export default function Header() {
   return (
-    <header className='bg-main h-full w-full'>
-      BizCard
+    <header className='bg-main flex items-center justify-between h-full'>
+      <div className='w-80'></div>
+      <div className='text-white text-lg'>
+        BizCard
+      </div>
+      <div className='flex justify-end w-80'>
+        <div className='bg-white rounded cursor-pointer text-main text-sm font-bold mr-3 px-1 py-1/2 select-none'>利用ルール</div>
+      </div>
     </header>
   );
 }
