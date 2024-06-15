7dcbd57a1723a42cc5237731ac0a4b920f4c58f7
Garnet3106 <private@garnet.works>
2024-06-15 11:21:25 +0900

    Fix template list UI

--- src/components/template/item.tsx
+++ src/components/template/item.tsx
@@ -1,6 +1,20 @@
-export default function TemplateItem() {
+import { Template } from '@/utils/template';
+import Image from 'next/image';
+
+export type TemplateItemProps = {
+  template: Template,
+};
+
+export default function TemplateItem(props: TemplateItemProps) {
   return (
-    <div className='bg-faded h-[200px] w-[30vw]'>
+    <div className='bg-faded h-[151px] w-[250px]'>
+      <Image
+        className='cursor-pointer select-none'
+        src={props.template.thumbnailUrl}
+        height={151}
+        width={250}
+        alt={props.template.name}
+      />
     </div>
   );
 }
--- src/components/template/list.tsx
+++ src/components/template/list.tsx
@@ -1,14 +1,21 @@
+import { useState } from 'react';
 import TemplateItem from './item';
+import { Template } from '@/utils/template';
 
 export default function TemplateList() {
+  const [templates, setTemplates] = useState<Template[]>([
+    { id: '0', name: '0000', thumbnailUrl: 'https://i.gyazo.com/b29ca8055b61a06ef25c198b164b5950.png' },
+    { id: '1', name: '0001', thumbnailUrl: 'https://i.gyazo.com/b29ca8055b61a06ef25c198b164b5950.png' },
+  ]);
+
   return (
     <div className='flex justify-center'>
-      <div className='flex flex-wrap gap-3 justify-center'>
-        <TemplateItem />
-        <TemplateItem />
-        <TemplateItem />
-        <TemplateItem />
-        <TemplateItem />
+      <div className='flex flex-wrap gap-3 justify-center mx-3'>
+        {
+          templates.map((item) => (
+            <TemplateItem template={item} key={item.id} />
+          ))
+        }
       </div>
     </div>
   );
