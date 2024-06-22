b69e4ffba52c0e2b75fa0b557a42ea6bf6bcf48a
Garnet3106 <private@garnet.works>
2024-06-22 13:31:29 +0900

    Hide search area

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 'use client';
 
-import SearchArea from '@/components/search/area';
 import TemplateList from '@/components/template/list';
 
 export default function Home() {
   return (
     <main>
-      <div className="flex items-center justify-center h-[200px] w-full">
+      {/* <div className="flex items-center justify-center h-[200px] w-full">
         <SearchArea />
       </div>
       <div className='overflow-y-scroll scrollbar-none h-[calc(100vh-200px-50px-25px)]'>
+        <TemplateList />
+      </div> */}
+      <div className='overflow-y-scroll scrollbar-none py-10 h-[calc(100vh-50px)]'>
         <TemplateList />
       </div>
     </main>
